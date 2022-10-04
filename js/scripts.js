fb7504a84a52926ce678908c1f6dd0e9ae8b32dc
CoderRocha <guilhermerochas0420@gmail.com>
2022-10-04 14:58:51 -0300

    add submit form script

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,23 @@ function mostrarMenu() {
     }
     event.preventDefault();
 }
+
+var form = document.getElementById('form');
+
+function clickLogin() {
+    if (form.checkValidity()) {
+        alert("Login realizado com sucesso!");
+    }
+}
+
+function clickRegister() {
+    if (form.checkValidity()) {
+        alert("Registro realizado com sucesso!");
+    }
+}
+
+function clickContact() {
+    if (form.checkValidity()) {
+        alert("Formulário enviado com sucesso!");
+    }
+}
