6dfb21d6c1a412b6b3069a0f5e433319bb8cea92
CoderRocha <guilhermerochas0420@gmail.com>
2022-10-06 21:28:29 -0300

    add jquery and fixed scripts section

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,19 @@ function mostrarMenu() {
     event.preventDefault();
 }
 
-var form = document.getElementById('form');
+/* function mostrarMenuJquery() {
+    if ($("#menu").hasClass("menu-jquery-escondido")) {
+        $("#menu").addClass("menu-jquery")
+        $("#menu").removeClass("menu-jquery-escondido")
+    } else {
+        $("#menu").addClass("menu-jquery-escondido")
+        $("#menu").removeClass("menu-jquery")
+    }
+
+    $("#menu").toggle()
+}*/
+
+let form = document.getElementById('form');
 
 function clickLogin() {
     if (form.checkValidity()) {
@@ -28,4 +40,79 @@ function clickContact() {
     if (form.checkValidity()) {
         alert("Formulário enviado com sucesso!");
     }
+}
+
+/* DOM -> Document Object Model */
+function renderizarProdutos() {
+    let secaoProdutos = document.getElementById('secao_produtos')
+
+    let listaProdutos = [
+        {
+            nome: "Xbox One",
+            imagem: "./img/xboxone.jpg",
+            descricao: "Descrição",
+            preco: 2500.00
+        },
+        {
+            nome: "Playstation 4",
+            imagem: "./img/ps4.jpg",
+            descricao: "Descrição",
+            preco: 2500.00
+        },
+        {
+            nome: "Notebook Gamer Start",
+            imagem: "./img/notebookstart.jpg",
+            descricao: "Descrição",
+            preco: 3500.00
+        },
+        {
+            nome: "Xbox One",
+            imagem: "./img/xboxone.jpg",
+            descricao: "Descrição",
+            preco: 2500.00
+        },
+        {
+            nome: "Playstation 4",
+            imagem: "./img/ps4.jpg",
+            descricao: "Descrição",
+            preco: 2500.00
+        },
+        {
+            nome: "Notebook Gamer Start",
+            imagem: "./img/notebookstart.jpg",
+            descricao: "Descrição",
+            preco: 3500.00
+        },
+        {
+            nome: "Xbox One",
+            imagem: "./img/xboxone.jpg",
+            descricao: "Descrição",
+            preco: 2500.00
+        },
+        {
+            nome: "Playstation 4",
+            imagem: "./img/ps4.jpg",
+            descricao: "Descrição",
+            preco: 2500.00
+        }
+    ]
+
+    // Template Strings (usa crase como separador ``)
+    let Template = ""
+
+    for (let index = 0; index < listaProdutos.length; index++) {
+        template = template + `
+        <div class="produtos card">
+            <a href=""><img class="imagemprod" src="${ listaProdutos[index].imagem }"></a>
+            <h4>${ listaProdutos[index].nome }</h4>
+            <p>${ listaProdutos[index].descricao }</p>
+            <p>R$${ listaProdutos[index].preco }</p>
+            <a href="">
+            <h3 class="button">Comprar</h3>
+            </a>
+        </div>
+        `
+    }
+
+    secaoProdutos.innerHTML = template
 }
